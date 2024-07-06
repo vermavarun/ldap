f1d92a4aa1ad62c05b7d84827ea30fae3de7b245
Varun Verma <vermavarun@outlook.com>
2024-07-06 22:16:12 +0530

    ps

---
+++ ldap/connect.ps1
@@ -0,0 +1,14 @@
+﻿$username = "pu*"
+$password = "Gg****"
+
+$ldapPath = "LDAP://169.***.249.***:389"
+
+$directoryEntry = New-Object System.DirectoryServices.DirectoryEntry($ldapPath, $username, $password)
+
+try {
+    $directoryEntry.RefreshCache()
+    Write-Host "ok"
+}
+catch {
+    Write-Host "failed"
+}
